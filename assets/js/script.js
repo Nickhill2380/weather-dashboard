3f9aebd9b22d5c9c4d209e6e4068da10a032a565
Nick Hill <nickhill2380@gmail.com>
2020-11-19 15:46:38 -0500

    allowed for searching by city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+var citySearchEl = document.querySelector("#city-search");
+var cityNameEl = document.querySelector("#city-name");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    var cityName = cityNameEl.value.trim();
+
+    if (cityName) {
+        getDailyWeather(cityName);
+        cityNameEl.value="";
+    } else {
+        alert("Please enter a City name");
+    }
+}
+
+
 var getDailyWeather = function(city) {
     var cityWeather = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ecc3713fcc8bcf9a9b86ba74c250821b";
     
@@ -14,4 +31,6 @@ var getDailyWeather = function(city) {
     })
 }
 
-getDailyWeather("buffalo");
+//getDailyWeather("buffalo");
+
+citySearchEl.addEventListener("submit", formSubmitHandler);
