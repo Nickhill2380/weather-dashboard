15ac1c2d7132b4f2e9ffd053f72fb413f13302b2
Nick Hill <nickhill2380@gmail.com>
2020-11-19 15:30:05 -0500

    set up html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
-var getDailyWeather = function() {
-    console.log("function was called");
+var getDailyWeather = function(city) {
+    var cityWeather = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ecc3713fcc8bcf9a9b86ba74c250821b";
+    
+    fetch(cityWeather)
+    .then(function(response){
+        if (response.ok)  {
+            response.json().then(function(data) {
+                console.log(data);
+                console.log(data.main.temp)
+            });
+        } //else {
+            //alert("Error: " + response.statusText)
+        //}
+    })
 }
 
-getDailyWeather();
+getDailyWeather("buffalo");
